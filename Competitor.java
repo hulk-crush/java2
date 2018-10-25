a82d58f3c8f030b3d2433db0c804f782fd33a6ce
Nastya <anastasia.halk27@gmail.com>
2018-10-25 20:53:37 +0300

    maraphone

---
+++ Competitor.java
@@ -0,0 +1,13 @@
+package Marathon;
+
+public interface Competitor {
+    void run(int dist);
+
+    void swim(int dist);
+
+    void jump(int height);
+
+    boolean isOnDistance();
+
+    void info();
+}
